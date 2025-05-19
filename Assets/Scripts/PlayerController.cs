4809916e7aad255afef59f487924a5ddfebaa9a9
reda0206 <cjones7@ltu.edu>
2025-05-19 14:13:46 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+   
+   private void Update()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
